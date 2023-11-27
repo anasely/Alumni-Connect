c707e5b436f56e09dad990009453f499339d50af
anasely <58348015+anasely@users.noreply.github.com>
2023-11-27 14:37:38 +0100

    change api

--- authParams.js
+++ authParams.js
@@ -1,5 +1,5 @@
 export const params = {
-  api: "http://localhost/omk_thyp_24/omeka-s/api/",
+  api: "http://localhost/omk_thyp_23-24_clone/api/",
   key: "zCDoVczqJaPXxE2U8WAr0UFYILC0KrNk  ",
   ident: "SRSzlkuGkcLCePs7CF8AwnRPNt7FvsFR",
   mail: "anaselyassai@gmail.com",
